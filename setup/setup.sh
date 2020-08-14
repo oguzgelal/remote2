85f3e059933db3c5a3ecce5107a15f217cf1099a
Oguz Gelal <o.gelal77@gmail.com>
2020-08-13 23:08:37 -0400

    update start script. comments

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,6 +3,10 @@
 echo "Setting up..."
 apt-get update
 
+# TODO: no front-end issue - debconf: unable to initialize frontend: Dialog
+# TODO: -y flag for installation
+# Default shell setting not working for code-server
+
 # setup zsh & oh-my-zsh
 apt-get install zsh -y && \
   chsh -s $(which zsh) && \
--- start.sh
+++ start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+echo "Preparing..."
 apt-get update && apt-get install -y curl
 
 # install docker using the convenience script
+echo "Installing Docker..."
 sh -c "$(curl -fsSL https://get.docker.com)"
 
+# install docker-compose
+echo "Installing Docker Compose..."
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 # build & run the container
+echo "Starting..."
 docker-compose up -d
